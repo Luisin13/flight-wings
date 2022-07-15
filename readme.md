b3b7311905ed4912b8efe1aec14d1b0e59366eca
Luis <84397555+Luisin13@users.noreply.github.com>
2022-07-15 17:38:53 -0300

    Fixes & modrinth page

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ Just a simples mod that enables you to fly in survival mode.
 If you find any issues report in www.github.com/Luisin13/flight-wings/issues
 
 And if you want to contribute just create a pull request in www.github.com/Luisin13/flight-wings/pulls
+
+<a href="https://modrinth.com/mod/flightwings"> ![Download on modrinth](https://raw.githubusercontent.com/Prospector/badges/master/modrinth-badge-72h-padded.png)</a>
--- src/main/java/com/luis/flightwings/compat/jei/description/ItemsDescription.java
+++ src/main/java/com/luis/flightwings/compat/jei/description/ItemsDescription.java
@@ -5,6 +5,7 @@ import com.luis.flightwings.items.ModItems;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.registration.IRecipeRegistration;
 import mezz.jei.api.runtime.IIngredientManager;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.ItemStack;
 
@@ -14,6 +15,10 @@ public class ItemsDescription {
 
         IIngredientType<ItemStack> itemType = ingredientManager.getIngredientType(ItemStack.class);
 
-        registry.addIngredientInfo(new ItemStack(ModItems.FLIGHT_WING_GOLD.get()), itemType, new TranslatableComponent("description.flight_wings_gold"), ServerConfig.WINGS_EFFECT.get() ? ServerConfig.WINGS_EFFECT_CONSUME_XP.get() ? new TranslatableComponent("description.flight_wings_gold.ability", ServerConfig.WINGS_EFFECT_USE_XP.get()) : new TranslatableComponent("description.flight_wings_gold.ability.no_cost") : null);
+        registry.addIngredientInfo(new ItemStack(ModItems.FLIGHT_WING_GOLD.get()), itemType, new TranslatableComponent("description.flight_wings_gold"), ServerConfig.WINGS_EFFECT.get() ?
+                ServerConfig.WINGS_EFFECT_CONSUME_XP.get() ?
+                        new TranslatableComponent("description.flight_wings_gold.ability", ServerConfig.WINGS_EFFECT_USE_XP.get())
+                        : new TranslatableComponent("description.flight_wings_gold.ability.no_cost")
+                : new TextComponent(""));
     }
 }
